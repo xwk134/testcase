f3ccad3a6a02c988e9607aa42a7d4ad9b300804c
2148583859@qq.com <3437061763>
2022-09-15 23:45:46 +0800

    1.1.2.3.5

---
+++ src/testcase/Test5.java
@@ -0,0 +1,22 @@
+package testcase;
+
+public class Test5 {
+    public static void main(String[] args) {
+        long d1 = System.currentTimeMillis();
+        System.out.printf("%d阶乘的结果：%s%n", 10, factorial(10));
+        long d2 = System.currentTimeMillis();
+        System.out.printf("递归费时：%s%n", d2-d1);
+
+
+    }
+    static long factorial(int n) {
+        if(n==1) {
+            return 1;
+        }else{
+            return n*factorial(n-1);//n! = n * (n-1)!
+        }
+    }
+
+
+}
+fun = n(n-1)+n(n-2)
